896967661b07c0f8e4034a583353c37453877b08
Dr Blinken <drblinken@gmail.com>
2014-08-05 23:36:20 +0200

    elements draggable and editable

--- canjs/public/map.js
+++ canjs/public/map.js
@@ -12,21 +12,34 @@ $.fn.slider = function() {
         $el.css('top', 0);
         var dragging = false;
         var startY = 0;
+        var startX = 0;
         var startT = 0;
         $el.mousedown(function(ev) {
             dragging = true;
             startY = ev.clientY;
             startT = $el.css('top');
+            startX = ev.clientX;
+            startL = $el.css('left');
+            startL = parseInt(startL)
+            if (isNaN(startL)){
+              startL = 0
+            }
+           //$('#xy').append(startL);
+
         });
         $(window).mousemove(function(ev) {
             if (dragging) {
                 // calculate new top
                 var newTop = parseInt(startT) + (ev.clientY - startY);
-
+                var newLeft = parseInt(startL) + (ev.clientX - startX);
                 //stay in parent
                 var maxTop =  $el.parent().height()-$el.height();
+                var maxLeft = $el.parent().width()-$el.width();
                 newTop = newTop<0?0:newTop>maxTop?maxTop:newTop;
+                newLeft = newLeft<0?0:newLeft>maxLeft?maxLeft:newLeft;
+
                 $el.css('top', newTop );
+                $el.css('left', newLeft);
             }
         }).mouseup(function() {
             dragging = false;
